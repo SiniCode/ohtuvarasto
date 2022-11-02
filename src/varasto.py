816e7c81610dd709951a2520bc28a6f1d505e57f
Sini Arkko <sini.arkko@gmail.com>
2022-11-02 23:12:22 +0200

    Another try to break the code

--- src/varasto.py
+++ src/varasto.py
@@ -30,7 +30,7 @@ class Varasto:
 
     def ota_varastosta(self, maara):
         if maara < 0:
-            return 0.0
+            return
         if maara > self.saldo: 
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
@@ -42,4 +42,4 @@ class Varasto:
         return maara
     
     def __str__(self):
-        return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
+        return f"saldo = self.saldo, vielä tilaa {self.paljonko_mahtuu()}"
