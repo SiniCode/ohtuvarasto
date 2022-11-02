c6ceb6acff4f76143d6ff64c75512d6febd7afbf
Sini Arkko <sini.arkko@gmail.com>
2022-11-02 23:17:10 +0200

    Fix the code

--- src/varasto.py
+++ src/varasto.py
@@ -22,7 +22,7 @@ class Varasto:
 
     def lisaa_varastoon(self, maara):
         if maara < 0:
-            return maara
+            return
         if maara <= self.paljonko_mahtuu():
             self.saldo = self.saldo + maara
         else:
@@ -30,7 +30,7 @@ class Varasto:
 
     def ota_varastosta(self, maara):
         if maara < 0:
-            return
+            return 0.0
         if maara > self.saldo: 
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
@@ -42,4 +42,4 @@ class Varasto:
         return maara
     
     def __str__(self):
-        return f"saldo = self.saldo, vielä tilaa {self.paljonko_mahtuu()}"
+        return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
